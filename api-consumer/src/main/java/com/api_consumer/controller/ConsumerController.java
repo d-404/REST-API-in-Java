345edef899885334299fa009c8934a7b4f10503a
Souvik Das <souvik.404d@gmail.com>
2025-01-27 11:04:01 +0530

    Register Eureka server and added Swagger doc

--- api-consumer/src/main/java/com/api_consumer/controller/ConsumerController.java
+++ api-consumer/src/main/java/com/api_consumer/controller/ConsumerController.java
@@ -2,6 +2,8 @@ package com.api_consumer.controller;
 
 import java.util.List;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,28 +20,36 @@ import com.api_consumer.dto.Employee;
 @RequestMapping("/api/v1/consumer")
 public class ConsumerController {
 
+	private List<ServiceInstance> serviceInstance;
+	private String url;
+
+	public ConsumerController(DiscoveryClient discoveryClient) {
+		this.serviceInstance = discoveryClient.getInstances("REST-API");
+		this.url = serviceInstance.get(0).getUri().toString();
+	}
+
 	@GetMapping
 	public List<Employee> consumeAllEmployeesFromProducer() {
 		RestTemplate restTemplate = new RestTemplate();
-		return restTemplate.getForObject("http://localhost:8080/api/v1/emp", List.class);
+		return restTemplate.getForObject(url + "/api/v1/emp", List.class);
 	}
 
 	@PostMapping
 	public String createProfileConsumer(@RequestBody Employee employee) {
 		RestTemplate restTemplate = new RestTemplate();
-		return restTemplate.postForObject("http://localhost:8080/api/v1/emp", employee, String.class);
+		return restTemplate.postForObject(url + "/api/v1/emp", employee, String.class);
 	}
 
 	@PutMapping("/{uid}")
 	public void updateProfileConsumer(@PathVariable("uid") String uidEmail, @RequestBody Employee employee) {
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.put("http://localhost:8080/api/v1/emp/" + uidEmail, employee, String.class);
+		restTemplate.put(url + "/api/v1/emp/" + uidEmail, employee, String.class);
 		System.out.println("Updated record consumed successfully");
 	}
 
 	@DeleteMapping
 	public void deleteProfileConsumer(@PathVariable("uid") String uidEmail) {
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.delete("http://localhost:8080/api/v1/emp/" + uidEmail);
+		restTemplate.delete(url + "/api/v1/emp/" + uidEmail);
 	}
 }
