c9b22bf14b215182d793bb6a4e4ec8d697a83176
Souvik Das <souvik.404d@gmail.com>
2025-01-29 09:56:35 +0530

    Added api-gateway

--- oauth2/src/main/java/com/oauth2/controller/BankingController.java
+++ oauth2/src/main/java/com/oauth2/controller/BankingController.java
@@ -2,11 +2,13 @@ package com.oauth2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/v1")
 public class BankingController {
 
-	@GetMapping("listEmp")
+	@GetMapping("/listEmp")
 	public String empList() {
 		return "myEmployeeList.html";
 	}
